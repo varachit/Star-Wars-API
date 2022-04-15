d250f2ca099bd0bb5c67bdfef30a8ee7ed8a1433
Varachit W <doctors.csgo@gmail.com>
2022-04-16 01:21:31 +0700

    Update urls.py

--- backend/starwars/urls.py
+++ backend/starwars/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import starwars_home
+
 urlpatterns = [
+    path('', starwars_home),
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls'))
 ]
