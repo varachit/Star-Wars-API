766a8a31df0c009a3d4148684409e471a61c385c
Varachit W <doctors.csgo@gmail.com>
2022-04-16 23:30:12 +0700

    SWAPI Grabber Automation

--- py_client/detail.py
+++ py_client/detail.py
@@ -1,15 +1,15 @@
 import requests
 import json
 
-planet = []
+people = []
 count = 0
 hasNext = True
-endpoint = "https://swapi.dev/api/planets/"
+endpoint = "https://swapi.dev/api/people/"
 
 while hasNext:
     response = requests.get(endpoint)
     json_response = json.loads(response.text)
-    planet.extend(iter(json_response["results"]))
+    people.extend(iter(json_response["results"]))
     if json_response["next"] is None:
         count = json_response["count"]
         hasNext = False
@@ -17,7 +17,24 @@ while hasNext:
         nextEndpoint = json_response["next"]
         endpoint = nextEndpoint.replace('&format=json', '')
 
-print(f'Total number of planets in local: {len(planet)} | Actual number of planets in SWAPI: {count}')
-for eachPlanet in planet:
-    name = eachPlanet["name"]
-    residents = eachPlanet["residents"]
+print(f'Total number of people in local: {len(people)} | Actual number of people in SWAPI: {count}')
+for person in people:
+    name = person["name"]
+    homeworld = person["homeworld"]
+    starship = person["starships"]
+
+    print(f'Name: {name}')
+    if homeworld is not None:
+        each_homeworld_response = requests.get(homeworld)
+        print(f'Homeworld: {json.loads(each_homeworld_response.text)["name"]}')
+    else:
+        print('Homeworld: None\n')
+
+    print('Starship:')
+    if len(starship) > 0:
+        for each_starship_url in starship:
+            each_starship_response = requests.get(each_starship_url)
+            print(f'{json.loads(each_starship_response.text)["name"]}')
+    else:
+        print('None')
+    print('\n\n')
